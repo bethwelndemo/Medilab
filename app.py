d984275f8670bcd63e41ebb9d1162490c1aa8fa9
bethwelndemo <bethwelndemo2@gmail.com>
2024-05-27 11:45:09 +0300

    added membersignin

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 api = Api(app)
 
 # endpoints / routes
-from views.views import MemberSignup
+from views.views import MemberSignup, MemberSignin
 api.add_resource(MemberSignup, '/api/member_signup')
+api.add_resource(MemberSignin, '/api/member_signin')
 if __name__  == "__main__":
  
     app.run(debug=True)
--- views/views.py
+++ views/views.py
@@ -27,7 +27,7 @@ class MemberSignup(Resource):
             cursor = connection.cursor()
             # insert into db
             sql = "INSERT INTO member(surname, others, gender, email, phone, dob, status, password, location_id) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-            data = (surname, others,gender,email,phone,dob,status,password,location_id)
+            data = (surname, others,gender,email,phone,dob,status,hash_password(password),location_id)
             try:
                 cursor.execute(sql,data)
                 connection.commit()
@@ -38,3 +38,30 @@ class MemberSignup(Resource):
                 return jsonify({"Message":"Post failed. Member not saved"})
         else:
             return jsonify({"Message": response})
+        
+class MemberSignin(Resource):
+    def post(self):
+        # we want to get request from client
+        data = request.json
+        email = data["email"]
+        password = data["password"]
+        # connect to database
+        connection = pymysql.connect(host='localhost', user='root', password='', database='Medilab')
+        
+        # check if email exists
+        sql = "select * from member where email = %s"
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql, email)
+        if cursor.rowcount == 0:
+            return jsonify({"Message": "Email does not exist"})
+        else:
+            # check password
+            member = cursor.fetchone()
+            hashed_password = member["password"]
+            is_match_password = hash_verify(password, hashed_password)
+            if is_match_password == True:
+                return jsonify({"Message": "Login successful"})
+            elif is_match_password == False:
+                return jsonify({"Message": "Login failed"})
+            else:
+                return jsonify({"Message": "Something went wrong"})
